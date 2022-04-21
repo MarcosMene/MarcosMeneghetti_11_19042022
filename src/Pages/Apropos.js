6428ac7c031fb9f8c33effaf4dc3cc80f5f67bd4
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-21 18:53:43 +0200

    a propor page

--- src/Pages/Apropos.js
+++ src/Pages/Apropos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import AproposSection from "../components/AproposSection";
+import AproposCard from "../components/AproposSection";
 import Accordion from "../components/Accordion";
 import styled from "styled-components";
 
@@ -10,7 +10,7 @@ const Apropos = () => {
   return (
     <main className="main">
       <section className="container">
-        <AproposSection />
+        <AproposCard />
 
         <Accordions>
           <Accordion
@@ -18,24 +18,28 @@ const Apropos = () => {
             active={active}
             setActive={setActive}
             text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+            police="according-text large"
           />
           <Accordion
             title="Respect"
             active={active}
             setActive={setActive}
             text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            police="according-text large"
           />
           <Accordion
             title="Service"
             active={active}
             setActive={setActive}
             text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+            police="according-text large"
           />
           <Accordion
             title="Sécurité"
             active={active}
             setActive={setActive}
             text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            police="according-text large"
           />
         </Accordions>
       </section>
