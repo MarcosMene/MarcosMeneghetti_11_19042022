ebd34408dd2ecd36bac89ecea8b285711afb6225
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-21 19:17:17 +0200

    fetch.js file

---
+++ src/tools/Fetch.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function Fetch() {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+}
+
+export default Fetch;
