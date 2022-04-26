10da6b6b423c87cd4d469211c840cf21c0b5bb4d
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-26 15:37:25 +0200

    page logement avec accordeon, text, photo et rating

--- src/Pages/Logements.js
+++ src/Pages/Logements.js
@@ -1,17 +1,34 @@
 import React from "react";
-
+import { useParams, Navigate } from "react-router-dom";
+import { UseFetch } from "../tools/Services";
 import Carousel from "../components/Carousel";
 import LogementDescription from "../components/LogementDescription";
-import Test from "../components/test";
 
 const Logements = () => {
+  const urlParams = useParams();
+
+  const { data } = UseFetch("/data.json");
+
+  const housingData = data?.filter((data) => data.id === urlParams.id);
+
+ 
+
+  if (housingData === null) {
+    return <Navigate to="/404" />;
+  }
+
   return (
-    <main className="main">
-      <section className="container">
-        <Carousel />
-        <LogementDescription />
-        <Test />
-      </section>
+    <main className="main main-logements">
+      {housingData.map((location, index) => (
+        <section className="container" key={index}>
+          <Carousel key="carousel" />
+          <LogementDescription
+            key={location.title}
+            title={location.title}
+            location={location.location}
+          />
+        </section>
+      ))}
     </main>
   );
 };
