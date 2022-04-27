6daae39ca48dfb071b208be8b9ad8817a1188a36
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-27 15:56:23 +0200

    modification router 404

--- src/constant/router.js
+++ src/constant/router.js
@@ -2,4 +2,5 @@
 
 export const ROUTE_HOME = "/";
 export const ROUTE_ABOUT = "/about";
-export const ROUTE_HOUSING = "/housing/:id/";
+export const ROUTE_HOUSING = "/housing/:id";
+export const ROUTE_404 = "*";
