a2634376eef5e98cdba46c6de10891e6a2426117
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-27 16:08:53 +0200

    carousel finished

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,46 +1,124 @@
 import React from "react";
-import Carouselimage from "../assets/carousel.png";
+import { useState } from "react";
 import styled from "styled-components";
 import LeftArrow from "../assets/arrow-left.png";
 import RightArrow from "../assets/arrow-right.png";
+import { useParams, Navigate } from "react-router-dom";
+import { UseFetch } from "../tools/Services";
 
-const Carousel = () => {
+const Carousel = (id) => {
+  const [current, setCurrent] = useState(0);
+
+  const urlParams = useParams();
+
+  const { data } = UseFetch("/data.json");
+
+  const housingData = data?.filter((data) => data.id === urlParams.id);
+
+  const lengthArray = housingData.map((data) => data.pictures.length);
+
+  if (housingData === null) {
+    return <Navigate to="/*" />;
+  }
+
+  const nextSlide = () => {
+    setCurrent(current === lengthArray - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? lengthArray - 1 : current - 1);
+  };
+
   return (
-    <CarouselContainer>
-      <div className="carousel">
-        <img src={Carouselimage} alt="" />
-      </div>
-      <div>
-        <img className="arrow-left" src={LeftArrow} alt="" />
-        <img className="arrow-right" src={RightArrow} alt="" />
-      </div>
-      <div className="pagination">1/4</div>
-    </CarouselContainer>
+    <>
+      {housingData &&
+        housingData.map((location, index, id) => (
+          <CarouselContainer key={`${index}-${location.id}`}>
+            <div className="carousel">
+              {location.pictures.map((picture, index) => (
+                <div
+                  className={index === current ? "slide active" : "slide"}
+                  key={index}
+                >
+                  {index === current && (
+                    <img
+                      className="imageCarousel"
+                      src={picture}
+                      alt={location.title}
+                      key={`${index}-${id}`}
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <img
+              className={lengthArray > 1 ? "arrow-left" : "hide"}
+              src={LeftArrow}
+              alt="left arrow"
+              key={`${index}-${id}-arrow-left`}
+              onClick={prevSlide}
+            />
+
+            <img
+              className={lengthArray > 1 ? "arrow-right" : "hide"}
+              src={RightArrow}
+              alt="right arrow"
+              key={`${index}-${id}-arrow-right`}
+              onClick={nextSlide}
+            />
+
+            <div className="pagination">
+              {current + 1}/{lengthArray}
+            </div>
+          </CarouselContainer>
+        ))}
+    </>
   );
 };
 
 const CarouselContainer = styled.div`
   position: relative;
   margin-top: 20px;
-  width: 100%;
-  img {
-    width: 100%;
-    border-radius: 25px;
-  }
+
+  max-height: 400px;
 
   .arrow-left,
   .arrow-right {
     position: absolute;
-    top: 165px;
+    top: 40%;
     cursor: pointer;
-    width: 100px;
+    min-width: 20px;
+    max-width: 5%;
+
+    z-index: 10;
+    cursor: pointer;
+    user-select: none;
   }
 
   .arrow-left {
-    left: 0;
+    left: 10px;
   }
   .arrow-right {
-    right: 0;
+    right: 10px;
+  }
+
+  .slide {
+    opacity: 0;
+
+    transition-duration: 1s ease;
+  }
+
+  .slide.active {
+    opacity: 1;
+    transform: scale(1);
+    transition-duration: 1s;
+
+    .imageCarousel {
+      width: 100%;
+      height: 400px;
+      border-radius: 5px;
+      object-fit: cover;
+    }
   }
 
   .pagination {
@@ -50,6 +128,10 @@ const CarouselContainer = styled.div`
     font-size: 1.125rem;
     font-weight: 500;
     color: white;
+  }
+
+  .hide {
+    display: none;
   }
 `;
 
