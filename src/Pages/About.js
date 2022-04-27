59b278f2cf3b7dc96ec95af7086b473904e98247
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-27 10:33:04 +0200

    about page finished

---
+++ src/Pages/About.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+import Section from "../components/Section";
+import Accordion from "../components/Accordion";
+import styled from "styled-components";
+
+const Apropos = () => {
+  const [active, setActive] = useState("");
+
+  const accordionPhrases = [
+    {
+      title: "Fiabilité",
+      text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: "Sécurité",
+      text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
+  return (
+    <main className="main">
+      <section className="container">
+        <Section text={``} classname={`apropos-card`} />
+
+        <Accordions>
+          {accordionPhrases.map((phrase, index) => (
+            <Accordion
+              key={`${index}-${phrase.title}`}
+              title={phrase.title}
+              active={active}
+              setActive={setActive}
+              text={phrase.text}
+            />
+          ))}
+
+          {/* <Accordion
+            title="Fiabilité"
+            active={active}
+            setActive={setActive}
+            text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+            police="according-text large"
+          />
+          <Accordion
+            title="Respect"
+            active={active}
+            setActive={setActive}
+            text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            police="according-text large"
+          />
+          <Accordion
+            title="Service"
+            active={active}
+            setActive={setActive}
+            text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+            police="according-text large"
+          />
+          <Accordion
+            title="Sécurité"
+            active={active}
+            setActive={setActive}
+            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            police="according-text large"
+          /> */}
+        </Accordions>
+      </section>
+    </main>
+  );
+};
+
+const Accordions = styled.div`
+  margin-bottom: 150px;
+`;
+
+export default Apropos;
