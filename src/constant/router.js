c028cbc6113ef1111df011e75ec42946682cd5d6
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-04-30 10:51:14 +0200

    router final

--- src/constant/router.js
+++ src/constant/router.js
@@ -1,6 +1,6 @@
 // Global variables for Routes
 
 export const ROUTE_HOME = "/";
-export const ROUTE_ABOUT = "/about";
+export const ROUTE_ABOUT = "/about/";
 export const ROUTE_HOUSING = "/housing/:id";
 export const ROUTE_404 = "*";
