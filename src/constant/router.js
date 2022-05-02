7652d84c2d19f55884c408cd7af79f28e1ef65a6
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-05-02 17:03:52 +0200

    routers

--- src/constant/router.js
+++ src/constant/router.js
@@ -1,10 +1,3 @@
-// Global variables for Routes
-
-// export const ROUTE_HOME = "/";
-// export const ROUTE_ABOUT = "/about/";
-// export const ROUTE_HOUSING = "/housing/:id";
-// export const ROUTE_404 = "/*";
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
