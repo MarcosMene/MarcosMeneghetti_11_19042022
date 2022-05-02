9c05c602be2072be65f87c4d06b9ae239b3501e2
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-05-02 16:17:49 +0200

    correction routes

--- src/constant/router.js
+++ src/constant/router.js
@@ -1,6 +1,24 @@
 // Global variables for Routes
 
-export const ROUTE_HOME = "/";
-export const ROUTE_ABOUT = "/about/";
-export const ROUTE_HOUSING = "/housing/:id";
-export const ROUTE_404 = "/*";
+// export const ROUTE_HOME = "/";
+// export const ROUTE_ABOUT = "/about/";
+// export const ROUTE_HOUSING = "/housing/:id";
+// export const ROUTE_404 = "/*";
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Housing from "../Pages/Housings";
+import Page404 from "../Pages/404";
+
+const routesPath = (
+  <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/housing/:id" element={<Housing />}></Route>
+    <Route path="*" element={<Page404 />} />
+  </Routes>
+);
+
+export default routesPath;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  ROUTE_ABOUT,
-  ROUTE_HOME,
-  ROUTE_HOUSING,
-  ROUTE_404,
-} from "./constant/router";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/generalstyle.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Housing from "./Pages/Housings";
-import Page404 from "./Pages/404";
+import routesPath from "./constant/router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route exact path={ROUTE_HOME} element={<Home />} />
-        <Route path={ROUTE_ABOUT} element={<About />} />
-        <Route path={ROUTE_HOUSING} element={<Housing />}></Route>
-        <Route path={ROUTE_404} element={<Page404 />} />
-      </Routes>
+      {routesPath}
       <Footer />
     </BrowserRouter>
   </React.StrictMode>
