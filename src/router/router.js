44d6982bbf10f1ced6a19f3aaae74902adf923cd
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-05-03 12:22:56 +0200

    rename folder router

---
+++ src/router/router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Housing from "../Pages/Housings";
+import Page404 from "../Pages/Page404";
+
+const routesPath = (
+  <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/housing/:id" element={<Housing />}></Route>
+    <Route path="*" element={<Page404 />} />
+  </Routes>
+);
+
+export default routesPath;
